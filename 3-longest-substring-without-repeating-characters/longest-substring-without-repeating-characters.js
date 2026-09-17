7c9f2c42269426ca5ae85dc8b94f5144a3823bdd
Vishal Gupta <vishalgupta28012004@gmail.com>
2026-09-17 15:33:00 +0530

    Time: 68 ms (56.31%) | Memory: 62.1 MB (51.89%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -5,21 +5,20 @@
 var lengthOfLongestSubstring = function (s) {
     const obj = {};
     let left = 0;
-    let right = 0;
-    let maxSubstringNum = 0;
-    let currMaxStringNum = 0;
+    let maxLength = 0;
 
-    while (right < s.length) {
-        if (obj[s[right]]) {
-            obj[s[left]]--;
-            left++;
-        } else {
-            obj[s[right]] = 1;
-            currMaxStringNum = right - left + 1;
-            maxSubstringNum = Math.max(currMaxStringNum, maxSubstringNum);
-            right++;
+    for (let right = 0; right < s.length; right++) {
+        const char = s[right];
+        if (obj[char] !== undefined) {
+            left = Math.max(left, obj[char] + 1);
         }
+
+        obj[char] = right;
+        const currentLength = right - left + 1;
+
+        maxLength = Math.max(maxLength, currentLength);
     }
-    console.log("maxSubstringNum", maxSubstringNum);
-    return maxSubstringNum
+
+    console.log(maxLength);
+    return maxLength
 };
