620d36c06c4dac0e20ef96897bda7ac7bc841551
Vishal Gupta <vishalgupta28012004@gmail.com>
2026-09-20 15:43:50 +0530

    Time: 306 ms (5.01%) | Memory: 60.3 MB (19.14%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
@@ -4,30 +4,29 @@
  * @return {number}
  */
 var characterReplacement = function (s, k) {
-
-    const obj = {};
-    let right = 0;
     let left = 0;
-    let maxSubString = 0;
-    let maxFreq = 0;
+    let obj = {};
+    let maxfreqCurrentWindow = 0;
+    let MaxSubStringReplacement = 0;
 
-    while (right < s.length) {
-        obj[s[right]] = (obj[s[right]] || 0) + 1; // current window freq
-        const currentWindow = right - left + 1; // current window
-        maxFreq = Math.max(maxFreq, obj[s[right]]); // max freq in current window
-        const replaceableNumber = currentWindow - maxFreq; // how many can be replaceable in current window
-        // console.log("currentWindow", currentWindow);
-        // console.log("obj", obj);
-        // console.log("maxFreq", maxFreq);
-        // console.log("replaceableNumber", replaceableNumber);
-        if (k < replaceableNumber) {
-            // console.log("replaceableNumber greater than k");
+    for (let right = 0; right < s.length; right++) {
+        const currChar = s[right];
+        obj[currChar] = (obj[currChar] || 0) + 1;
+
+        let currentWindow = right - left + 1;
+        maxfreqCurrentWindow = Math.max(...Object.values(obj));
+        let replaceableChar = currentWindow - maxfreqCurrentWindow;
+
+        while (replaceableChar > k) {
             obj[s[left]]--;
             left++;
+
+            currentWindow = right - left + 1;
+            maxfreqCurrentWindow = Math.max(...Object.values(obj));
+            replaceableChar = currentWindow - maxfreqCurrentWindow;
         }
-        maxSubString = Math.max(maxSubString, right - left + 1);
-        right++;
+
+        MaxSubStringReplacement = Math.max(MaxSubStringReplacement, currentWindow);
     }
-    console.log("maxSubString", maxSubString);
-    return maxSubString
+    return MaxSubStringReplacement
 };
