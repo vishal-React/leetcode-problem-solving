3684521e3768d3c872fb8e28da59521171bd199d
Vishal Gupta <vishalgupta28012004@gmail.com>
2026-09-24 18:55:38 +0530

    Time: 604 ms (5.05%) | Memory: 65 MB (5.32%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+var minWindow = function (s, t) {
+    if (t.length > s.length) return "";
+
+    function validWindow(objTargetFreq, currWindowFreq) {
+        // console.log("objTargetFreq", objTargetFreq);
+        // console.log("currWindowFreq", currWindowFreq);
+        for (const key in objTargetFreq) {
+            if (objTargetFreq[key] > currWindowFreq[key] || !currWindowFreq[key]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    const objTargetFreq = {};
+    for (const char of t) {
+        objTargetFreq[char] = (objTargetFreq[char] || 0) + 1;
+    }
+
+    let left = 0;
+    let currWindowFreq = {};
+    let minimumWindowChar = "";
+
+    for (let right = 0; right < s.length; right++) {
+        currWindowFreq[s[right]] = (currWindowFreq[s[right]] || 0) + 1;
+        while (validWindow(objTargetFreq, currWindowFreq)) {
+            // console.log("first", right);
+            // console.log("s.slice(left, right + 1)", s.slice(left, right + 1));
+            if (
+                !minimumWindowChar.length ||
+                minimumWindowChar.length > right - left + 1
+            ) {
+                minimumWindowChar = s.slice(left, right + 1);
+            }
+            currWindowFreq[s[left]]--;
+            left++;
+        }
+    }
+    return minimumWindowChar;
+};
